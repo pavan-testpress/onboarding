ea989488a2ba0f06cb447dd824773f46c92edf02
Pavan Kumar <Pavan@testpress.in>
2019-01-09 07:46:00 +0530

    Add logout view - Add logout view - Add TestCase - Add logout link in template

--- onboarding/apps/accounts/urls.py
+++ onboarding/apps/accounts/urls.py
@@ -7,5 +7,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(authentication_form = UserLoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('', views.home, name='home'),
 ]
