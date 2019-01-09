a441c15cb64075de1262aac496aaacf11ce5b83e
pavan-testpress <45391567+pavan-testpress@users.noreply.github.com>
2019-01-09 11:12:01 +0530

    Add logout view (#2)
    - Add logout view
    - Add TestCase
    - Add logout link in template

---
+++ onboarding/tests/test_accounts/test_logout.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+from apps.accounts.models import User
+
+
+class AccountsTestCase(TestCase):
+
+    def setUp(self):
+        user = {
+            'first_name': 'Pavan Kumar',
+            'last_name': 'Kuppala',
+            'username': 'pavankumar',
+            'email': 'pavancse17@gmail.com',
+            'password1': '143Pavan..',
+            'password2': '143Pavan..',
+
+        }
+        User.objects.create_user(first_name=user['first_name'], last_name=user['last_name'],
+                                 username=user['username'], email=user['email'],
+                                 is_active=True, password='143Pavan..')
+
+    def test_logout(self):
+        self.client.login(username="pavankumar", password="143Pavan..")
+        response = self.client.get(reverse('accounts:logout'), follow=True)
+        self.assertRedirects(response, '/login/?next=/')
