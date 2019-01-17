a7670305917c5b8662cbeb2975b9940635e12340
Pavan Kumar <Pavan@testpress.in>
2019-01-17 17:02:10 +0530

    Add FolderUpdateView
    - Added folder_update_form template.
    - Added FolderUpdateView.
    - Added FolderUpdateForm.
    - Added test_folder_edit testcases.

--- onboarding/apps/bookmarks/urls.py
+++ onboarding/apps/bookmarks/urls.py
@@ -7,6 +7,7 @@ app_name = "bookmarks"
 urlpatterns = [
     path('', views.FolderListView.as_view(), name="folders"),
     path('create/', views.FolderCreateView.as_view(), name="folder_create"),
+    path('<slug>/edit/', views.FolderUpdateView.as_view(), name="folder_update"),
     path('<slug>/', views.BookmarkListView.as_view(), name="bookmarks"),
     path('<slug>/create/', views.BookmarkCreateView.as_view(), name="bookmark_create"),
 ]
